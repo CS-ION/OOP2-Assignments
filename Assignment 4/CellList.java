2b6eea3586fd5bef5c44ea4448c9986a1e0a1e3d
Ayaan Jilani <59445725+CS-ION@users.noreply.github.com>
2023-03-20 22:19:36 -0400

    Update CellList.java

--- Assignment 4/CellList.java
+++ Assignment 4/CellList.java
@@ -2,14 +2,7 @@ import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 /**
- * Mateo Nieto (40192918) and Syed Ayaan Jilani (40209519)
- * COMP249
- * Assignment 3 Part II
- * 7th October 2022
- */
-
-/**
- * @author Syed Ayaan Jilani (40209519) and Mateo Nieto (40192918)
+ * @author Syed Ayaan Jilani and Mateo Nieto
  */
 public class CellList {
 	
